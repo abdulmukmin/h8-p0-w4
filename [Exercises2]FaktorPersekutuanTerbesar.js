be769d885edd4d7bf9623d4d3e347befd2c62845
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-19 23:17:30 +0700

    Perubahan indikator pembanding saat looping agar mendapatkan nilai yang benar

--- [Exercises2]FaktorPersekutuanTerbesar.js
+++ [Exercises2]FaktorPersekutuanTerbesar.js
@@ -1,12 +1,12 @@
 function fpb(angka1, angka2) {
     var result = 0;
     
-    for(var a=1; a<angka1; a++){
+    for(var a=1; a<angka2; a++){
       if(angka1%a ===0 && angka2%a ===0) { 
         result = a;
       }
     }
-    return fpb;
+    return result;
   }
   
   // TEST CASES
@@ -15,4 +15,4 @@ function fpb(angka1, angka2) {
   console.log(fpb(22, 99)); // 11
   console.log(fpb(24, 36)); // 12
   console.log(fpb(17, 23)); // 1
-  
+  console.log(fpb(100, 200))
