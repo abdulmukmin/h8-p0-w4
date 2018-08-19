15124e18c68eda0ab2be9f1081cd32629bb3ffb0
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-19 23:07:49 +0700

    Perbaikan tugas 14, pengambilan nomor terbesar berdasarkan hasil sort manual dan kemudian ditampilkan, bukan berdasarkan posisi

--- [Exercises14]MostFrequentLargestNumbers.js
+++ [Exercises14]MostFrequentLargestNumbers.js
@@ -6,37 +6,28 @@ function getTotal(arrNumber) {
     //console.log('ini sudah di getTotal '+arrNumber)
     //console.log(arrNumber.length)
     //cari huruf2 yang ada, buat variabel baru 
-    var numUtama=[]
-    var terbesar=0
-    var indexDicari=0
-    for(i=0; i<arrNumber.length; i++){
-        if (arrNumber[i]!==arrNumber[i+1]){
-            numUtama.push(arrNumber[i])
+    var angkaTerbesar=0
+    var jumlahTerbesar=0
+    for (i=0; i<arrNumber.length;i++){
+        if (arrNumber[i] > angkaTerbesar){
+            angkaTerbesar = arrNumber[i]
         }
+    } //console.log(terbesar)-> sudah dapat nomor terbesar, sekarang cari indexnya
+    
+    //Filter dulu, kalo arraynya kosong, maka tampilkan empty stirng, selain itu lanjutkan
+    if (angkaTerbesar ===0 ){
+        return ''
     }
-    //ambil nomor terbesar di ujung kanan karena sudah di sort
-    terbesar=numUtama[numUtama.length-1]
-    indexTerbesar=numUtama.length-1
+    //looping jumlah huruf terbesar yang ada di dalam array
+    for (i=0; i<arrNumber.length; i++){
+        if (angkaTerbesar===arrNumber[i]){
+            jumlahTerbesar = jumlahTerbesar + 1
+        }//tampilkan banyaknya jumlah huruf dalam 'numItung' dari huruf terbesar dalam 'numUtama'
 
-    //hitung banyak setiap hurufnya
-    var numItung=[]
-    for(i=0; i<numUtama.length; i++){
-        var sementara=0 //-> agar nilainya tereset dari 0 setiap menghitung nomor yang lain
-        for(j=0; j<arrNumber.length; j++){
-            if(numUtama[i]===arrNumber[j]){
-                //console.log(numUtama[i]+' '+arrNumber[j])    
-                sementara = sementara + 1 
-            }
-        }
-        numItung.push(sementara)
-    }
-    //tampilkan banyaknya jumlah huruf dalam 'numItung' dari huruf terbesar dalam 'numUtama'
-    jumlahTerbesar=numItung[indexTerbesar]
-    if (jumlahTerbesar===undefined){
-        return '' //-> kenapa gak bisa difilter dari awal ya? pas ditaro di sorting dia gak mau ditambahin arrNumber.length
-    } else {
-        return 'angka paling besar adalah '+terbesar+' dan jumlah kemunculan sebanyak '+jumlahTerbesar+' kali'
-    }
+    } //console.log(jumlahTerbesar) -> sudah dapat jumlah terbesar, 
+
+    
+    return 'angka paling besar adalah '+angkaTerbesar+' dan jumlah kemunculan sebanyak '+jumlahTerbesar+' kali'
 }
   
 function mostFrequentLargestNumbers(arrNumber) {
@@ -45,14 +36,16 @@ function mostFrequentLargestNumbers(arrNumber) {
     return countHighest;
 }
   
-  console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
-  //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+   console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+//   //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
   
-  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
-  //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+   console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+//   //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
   
-  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
-  //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+   console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+//   //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
   
-  console.log(mostFrequentLargestNumbers([]));
-  //''
+   console.log(mostFrequentLargestNumbers([]));
+//   //''
+
+  console.log(mostFrequentLargestNumbers( [10, 2, 8, 4, 6, 8, 5, 8, 4]));
