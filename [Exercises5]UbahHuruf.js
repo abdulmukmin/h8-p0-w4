2ac3d67c22c17b26ce129a2023a71a2b8cd3cd03
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-19 23:11:08 +0700

    Perbaikan output untuk huruf input 'z' dengan menampilkan 'a'

--- [Exercises5]UbahHuruf.js
+++ [Exercises5]UbahHuruf.js
@@ -4,7 +4,11 @@ function ubahHuruf(kata) {
     //ketahui posisi index huruf yang ada di kata sekarang, gampangnya pake findindex tapi manualnya :
     for (a=0;a<kata.length;a++){
         for (b=0;b<huruf.length;b++){
-            if (huruf[b]===kata[a]){
+            //bila huruf yang diinput adalah 'z', maka berikan huruf 'a'
+            if (huruf[b]===kata[a]&&kata[a]==='z'){
+                //tambahkan index huruf saat ini dengan 1 agar mendapatkan huruf setelahnya
+                kataBaru += huruf[0]
+            } else if (huruf[b]===kata[a]){
                 //tambahkan index huruf saat ini dengan 1 agar mendapatkan huruf setelahnya
                 kataBaru += huruf[b+1]
             }
