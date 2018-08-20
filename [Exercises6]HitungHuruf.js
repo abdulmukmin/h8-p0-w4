7a5f230d1e125ef6271f199610300b0b426ec5d4
Abdul Mu'min <abdmukmin@rocketmail.com>
2018-08-20 17:48:25 +0700

    Perbaikan penampakan nilai array yang terbesar yang pertama kali muncul

--- [Exercises6]HitungHuruf.js
+++ [Exercises6]HitungHuruf.js
@@ -1,61 +1,88 @@
 function hitungHuruf(kata) {
-    // pisahkan masing-masing kalimat ke dalam array, gunakan split saja, bisa juga dengan manual, ada di exercises week 3
-    hurufKecil = kata.toLowerCase()
-    pisahKata= hurufKecil.split(' ') //-> string kalo di split otomatis jadi array?
-    var perHuruf=[]
-    // pisahkan setiap huruf dalam kata, tetapi tetap jadikan di satu index perkata
-    // console.log(pisahKata.length) -> tes
-    // looping ke-1 untuk mengambil 1 kata
-    for (a=0;a<pisahKata.length;a++){
-        // looping ke-2 untuk mengambil setiap huruf dan meletakannya dalam array di kata yang sama
-        perHuruf.push([]) //-> buat array baru buat dimasukkan array perkata
-        //console.log(pisahKata[a].length) -> tes kata
-        for(b=0;b<pisahKata[a].length;b++){
-            //console.log(pisahKata[a][b]) -> tes huruf
-            perHuruf[a].push(pisahKata[a][b]) //-> jangan lupa index array dituliskan juga agar bisa dimasukkan perindex
-        }//console.log(perHuruf)
-    } 
-    //buat looping untuk sort lalu dibandingkan angka pertama sampai dengan angka terakhir, bila sama ditambahkan 1
-    var jumlahHuruf=[]
-    for (a=0; a<perHuruf.length; a++){
-        //jumlahHuruf.push([])
-        perHuruf[a].sort()
-        //console.log(perHuruf[a])
-        var hitungHuruf = 0
-        var hurufSekarang = ''
-        for (b=0;b<perHuruf[a].length;b++){
-            if(perHuruf[a][b]===perHuruf[a][b+1]){
-                //console.log(perHuruf[a][b],'sama dengan',perHuruf[a][b+1])
-                //hurufSekarang=perHuruf[a][b]
-                hitungHuruf=hitungHuruf+1
-                
-            } else {
-                //console.log('sama')
-                //hitungHuruf=hitungHuruf+1
-                //jumlahHuruf[a].push([hitungHuruf])
-            }
-        }jumlahHuruf.push(hitungHuruf)
-    }//console.log(jumlahHuruf)
-    //Sudah dapat index lokasi kata yang banyak doublenya, bandingkan. 
-    var hurufTerbanyak = 0
-    var indexHurufTerbanyak =0
-    for(a=0; a<jumlahHuruf.length; a++){
-        if(hurufTerbanyak<jumlahHuruf[a]){
-            hurufTerbanyak=jumlahHuruf[a]
-            indexHurufTerbanyak=a
+    //pecah kalimat menjadi kata
+    var sementara=''
+    var kalimatArr = []
+    var penampungObj=[]
+    // console.log(kata)
+    for (i=0; i<kata.length; i++){
+        if(kata[i]===' '){
+            kalimatArr.push(sementara)
+            sementara=''
+            penampungObj.push({}) //-> buat persiapan nampung objek perkata nanti
+        } else {
+            sementara += kata[i]   
         }
-    } 
+    }
+    kalimatArr.push(sementara)
+    penampungObj.push({})
+    // console.log(penampungObj)
+    console.log(kalimatArr)
+    //----------------------------------//
+    //hitung huruf per kata
+    for (i=0; i<kalimatArr.length; i++){
+        for (j=0; j<kalimatArr[i].length; j++){
+            //console.log(kalimatArr[i][j])
+            //penampungObj[i][kalimatArr[i][j]] = 1 //-> contoh
+            if(penampungObj[i][kalimatArr[i][j]]===undefined){
+                penampungObj[i][kalimatArr[i][j]]=1 //->
+            } else {
+                penampungObj[i][kalimatArr[i][j]]+=1
+            }
+        }
+    }
+    //console.log(penampungObj)
+    //----------------------------------//
+    //console.log(Object.values(penampungObj[?])[??]) //-> melihat isi value dalam objek perindex array '?' dan index dari dalam array itu '??'
+    //cek berapa huruf terbanyak yang dimiliki oleh kata tersebut
+    //bila nilai pada huruf yang dilooping > 1 maka tambahkan 1
+    var angkaTerbanyak = []
+    
+    for (i=0; i<penampungObj.length; i++){
+        var terbanyakSementara =0
+        angkaTerbanyak.push([])
+        var objSekarang = penampungObj[i] //-> menghitung panjang objek di dalam array (1)
+        for (j=0; j<Object.keys(objSekarang).length; j++){
+            console.log(Object.values(penampungObj[i])[j])
+            //bila terdapat huruf yang banyaknya lebih dari 1, masukkan dalam array angka terbanyak
+            if (Object.values(penampungObj[i])[j]>1){
+                terbanyakSementara+=Object.values(penampungObj[i])[j]
+                
+            } 
+        }angkaTerbanyak[i].push(terbanyakSementara) //-> abis diitung baru di push
+    } console.log(angkaTerbanyak)
+    //----------------------------------//
+    //cari index terbanyaknya-----------//
+    var indexHurufTerbanyak=0
+    for (i=0; i<angkaTerbanyak.length; i++){
+        console.log(angkaTerbanyak[i])
+        if(indexHurufTerbanyak<angkaTerbanyak[i]){
+            indexHurufTerbanyak =angkaTerbanyak[i]
+            console.log(indexHurufTerbanyak,' dan',angkaTerbanyak[i])
+        }
+    } console.log(indexHurufTerbanyak)
+
+    //----------------------------------//
     //Bila ada kata yang tidak dobule maka return -1
-    if(hurufTerbanyak===0){
+    if (indexHurufTerbanyak === 0) {
         return -1
     } else {
-        return (pisahKata[indexHurufTerbanyak]) 
+            //cari isi kata dari 'indexHurufTerbanyak'
+            for (i=0; i<angkaTerbanyak.length; i++){
+                console.log(angkaTerbanyak[i])
+                //looping nilai dalam 'angka terbanyak' sampai ditemukan nilai yang sama dengan indexterbanyak
+                if (angkaTerbanyak[i]===indexHurufTerbanyak){
+                    //tampilkan nilai dari posisi pertama ditemukan indexterbanyak
+                    //console.log(angkaTerbanyak[i])
+                    return kalimatArr[i]
+                }
+            }
     }
-  }
-  
-  // TEST CASES
- console.log(hitungHuruf('Today, is the greatest day ever' )); // greatest
- console.log(hitungHuruf('I am a passionate developer')); // passionate
- console.log(hitungHuruf('aku adalah anak gembala')); // adalah console.log(hitungHuruf('greatest day ever' )); // greatest
- console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
- console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+}
+
+// TEST CASES
+  console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
+  console.log(hitungHuruf('I am a passionate developer')); // passionate
+  console.log(hitungHuruf('aku adalah anak gembala')); // adalah 
+  console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
+  console.log(hitungHuruf('mengayuh perahu di danau')); // danau
+  console.log(hitungHuruf('I am a developer passionate'))
